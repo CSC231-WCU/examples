38aa78fddafd98765c6daa04ab73825f439ae148
Linh Ngo <lngo@wcupa.edu>
2020-09-13 14:06:23 -0400

    add examples for floating point representation

--- data-representation/show_bytes.c
+++ data-representation/show_bytes.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
 #include <show_bytes.h>
   
 #define N 32
@@ -26,4 +27,32 @@ void decToBinary(int n) {
     }
   }
   printf("\n"); 
+}
+
+void floatToBinary(float f) {
+  union {
+    float f;
+    uint32_t u;
+  } float_unsized_int;
+
+  float_unsized_int.f = f; 
+  int i = sizeof f * CHAR_BIT;
+  printf(" ");  
+  while (i--) {
+    printf ("%d ", (float_unsized_int.u >> i) & 0x1);
+  }
+  putchar ('\n');
+  printf ("|- - - - - - - - - - - - - - - - - - - - - - "
+          "- - - - - - - - - -|\n");
+  printf ("|s|      exp      |                  mantissa"
+          "                   |\n\n");
+
+  i = sizeof(f) * CHAR_BIT;
+  while (i--) {
+    printf ("%d", (float_unsized_int.u >> i) & 0x1);
+    if (i % 4 == 0) {
+      printf(" ");
+    }
+  }
+  putchar('\n');
 } 
--- data-representation/show_bytes.h
+++ data-representation/show_bytes.h
@@ -1,1 +1,2 @@
 void decToBinary(int);
+void floatToBinary(float);
